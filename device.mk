aa9b8455a1c08b58f1245253cf14f38ace77d65a
LuK1337 <priv.luk@gmail.com>
2022-10-28 19:49:22 +0100

    earth: Enable `PRODUCT_USE_DYNAMIC_PARTITIONS`
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -39,6 +39,9 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
