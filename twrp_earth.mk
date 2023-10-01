3315e2a9301d41065df6cb6ada9b78dfe64a991d
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-10-01 22:43:51 +0530

    earth: Initial TWRP Bringup
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from earth device
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 PRODUCT_DEVICE := earth
-PRODUCT_NAME := omni_earth
+PRODUCT_NAME := earth_earth
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := 22120RN86C
 PRODUCT_MANUFACTURER := xiaomi
