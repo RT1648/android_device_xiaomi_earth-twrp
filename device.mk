09d5b88e2c0ea1cc2dd1aaf276e2af8790e10145
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-10-02 16:11:07 +0530

    earth: Fix build errors
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -63,10 +63,10 @@ PRODUCT_PACKAGES += \
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    libxml2
+    libxml2 \
     vendor.display.config@1.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so
